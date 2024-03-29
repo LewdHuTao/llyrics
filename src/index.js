8eabdc5d8837ad768ddb5f0b75d06ae3542d7269
LewdHuTao <imanimtiyaz242@gmail.com>
2024-03-29 22:40:01 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
-const cheerio = require("cheerio");
 
 async function findLyrics(apiKey, songName) {
+  
   let api_key = apiKey;
   let song_Name = songName;
   let lyrics;
@@ -9,55 +9,18 @@ async function findLyrics(apiKey, songName) {
   let trackArtist;
 
   try {
-    const searchResponse = await axios.get("https://api.genius.com/search", {
-      params: {
-        q: song_Name,
-      },
-      headers: {
-        Authorization: `Bearer ${api_key}`,
-      },
-    });
-
+    const searchResponse = await axios.get(`https://llyrics-api.vercel.app//lyrics?title=${song_Name}&geniusAPI=${api_key}`)
     if (searchResponse.status === 200) {
       const searchData = searchResponse.data;
-      if (
-        searchData.response &&
-        searchData.response.hits &&
-        searchData.response.hits.length > 0
-      ) {
-        trackName = searchData.response.hits[0].result.title;
-
-        trackArtist = searchData.response.hits[0].result.primary_artist.name;
-        
-        const songUrl = searchData.response.hits[0].result.url;
-
-        const lyricsResponse = await axios.get(songUrl);
-
-        if (lyricsResponse.status === 200) {
-          const lyricsHtml = lyricsResponse.data;
-
-          const $ = cheerio.load(lyricsHtml);
-
-          lyrics = $('div[class="lyrics"]').text().trim();
-          if (!lyrics) {
-            lyrics = "";
-            $('div[class^="Lyrics__Container"]').each((i, elem) => {
-              if ($(elem).text().length !== 0) {
-                let snippet = $(elem)
-                  .html()
-                  .replace(/<br>/g, "\n")
-                  .replace(/<(?!\s*br\s*\/?)[^>]+>/gi, "");
-                lyrics += $("<textarea/>").html(snippet).text().trim() + "\n\n";
-              }
-            });
-          }
-          if (!lyrics) throw new Error("No lyrics found");
-          findLyrics.lyrics = lyrics.trim();
+      lyrics = searchData.lyrics;
+      trackName = searchData.trackName;
+      trackArtist = searchData.trackArtist;
+          if (!lyrics) throw new Error("No lyrics found.");
+          findLyrics.lyrics = lyrics;
           findLyrics.trackName = trackName;
           findLyrics.trackArtist = trackArtist;
+          console.log(lyrics);
         }
-      }
-    }
   } catch (error) {
     throw new Error(error);
   }
